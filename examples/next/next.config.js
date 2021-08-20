0473bdcd93d1051141a11fb9de4ba03f0e26d7f7
Velten Heyn <github@valoric.de>
2021-08-20 11:37:15 +0200

    Adapt example/next to next 11.1.0

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -1,6 +1,6 @@
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
-function useEsbuildMinify(config, options) {
+function addEsbuildMinify(config, options) {
 	const terserIndex = config.optimization.minimizer.findIndex(minimizer => (minimizer.constructor.name === 'TerserPlugin'));
 	if (terserIndex > -1) {
 		config.optimization.minimizer.splice(
@@ -11,13 +11,29 @@ function useEsbuildMinify(config, options) {
 	}
 }
 
-function useEsbuildLoader(config, options) {
-	const jsLoader = config.module.rules.find(rule => rule.test && rule.test.test('.js'));
+function addEsbuildLoader(config, options) {
+	const jsRule = config.module.rules.find(rule => rule.test && rule.test.test('.tsx'));
+	if (!jsRule) throw new Error('No js Loader found!');
 
-	if (jsLoader) {
-		jsLoader.use.loader = 'esbuild-loader';
-		jsLoader.use.options = options;
+	if (config.name === 'client') {
+		if (
+			jsRule.use.length !== 2 ||
+			!jsRule.use[0].includes('@next/react-refresh-utils/loader.js') ||
+			!jsRule.use[1].loader?.includes('next/dist/build/babel/loader/index.js')
+		)
+			throw new Error('webpack\'s "config.module.rules.use" structure changed. Please adopt');
+
+		const use = jsRule.use[1]
+		use.loader = 'esbuild-loader';
+		use.options = options;
+		return;
 	}
+	if (config.name === 'server') {
+		jsRule.use.loader = 'esbuild-loader';
+		jsRule.use.options = options;
+		return;
+	}
+	throw new Error('Unknown build name');
 }
 
 module.exports = {
@@ -28,11 +44,11 @@ module.exports = {
 			}),
 		);
 
-		useEsbuildMinify(config);
+		addEsbuildMinify(config);
 
-		useEsbuildLoader(config, {
-			// Specify `tsx` if you're using TypeSCript
-			loader: 'jsx',
+		addEsbuildLoader(config, {
+			// Specify `jsx` if you're not using Typescript
+			loader: 'tsx',
 			target: 'es2017',
 		});
 
